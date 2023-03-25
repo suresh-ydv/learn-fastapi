c33bda7d2a69cbbcaa555d60cc34fda02662be73
suresh yadav <ydvsuresh40242@gmail.com>
2023-03-26 01:01:50 +0545

    Basic code with fastapi router

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+'''*** Basic code for well understanding ***'''
+
+
+class Sign_Up(BaseModel):
+    id: int
+    name: str
+    phone_no: str
+    description: Optional[str] = None
+    email: str
+    password: str
 
 
 @app.get("/")
@@ -8,3 +21,40 @@ async def root():
     return {"message": "Keep learning"}
 
 
+user_data = [{"id": 101,
+              "name": "Raina bhai",
+              "phone_no": "9804402354",
+              "description": "Here is the description",
+              "email": "ydvsuresh40242@gmail.com",
+              "password": "password"
+              }]
+
+
+@app.post("/signup")
+async def user_signup(items: Sign_Up):
+    # print(items)
+    # print(items.dict())
+    new_signup = items.dict()
+    user_data.append(new_signup)
+    # print(user_data)
+    return {"new_post": new_signup}
+
+
+@app.get("/user_data")
+async def user_details():
+    return {"data": user_data}
+
+
+def user_find(id):
+    for p in user_data:
+        if p["id"] == id:
+            return p
+
+
+@app.get("/find_user/{id}")
+async def find_user(id):
+    Details = user_find(int(id))
+    return {"UserDetails": Details}
+
+
+'''**** Try to focus on above code for basic understanding ***'''
